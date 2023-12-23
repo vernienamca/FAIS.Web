81b38ae21ad0bbd637f398b0ca8295dfc8a4f879
JMNietoatBusyBee <juan@mybusybee.net>
2023-12-23 14:17:49 +0800

    fix

--- src/app/pages/pages/auth/forgot-password/forgot-password.component.ts
+++ src/app/pages/pages/auth/forgot-password/forgot-password.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit,OnDestroy } from '@angular/core';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { fadeInUp400ms } from '../../../../../@vex/animations/fade-in-up.animation';
 import {FormGroup,FormBuilder} from '@angular/forms';
-//left arrow 
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { SecurityService } from 'src/app/core/services/security.service';
-
 import {  Subject,takeUntil } from 'rxjs';
 
 @Component({
@@ -17,18 +14,12 @@ import {  Subject,takeUntil } from 'rxjs';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  private _onDestroy$ = new Subject<void>();
-
-//captcha form group
+private _onDestroy$ = new Subject<void>();
 protected aFormGroup: FormGroup;
 
-
-//left arrow 
 icon = faArrowLeft;
 passwordSent = false;
 buttonText = 'SEND RECOVERY LINK';
-
-//Image URLS 
 imageUrl = 'assets/img/icons/forgot-password-icons/Vector.png';
 
 
@@ -40,7 +31,6 @@ public getJsonValue: any;
 public postJsonValue: any;
 
   constructor(
-    private router: Router,
     private fb: UntypedFormBuilder,
     private formBuilder: FormBuilder,
     private _securityService: SecurityService,
@@ -49,15 +39,9 @@ public postJsonValue: any;
   ) { }
 
   ngOnInit():void {
-
-    //captcha
 this.aFormGroup = this.formBuilder.group({
   recaptcha:['', Validators.required]
-
 })
-
-
-
   } 
 
   ngOnDestroy(): void {
@@ -65,18 +49,11 @@ this.aFormGroup = this.formBuilder.group({
     this._onDestroy$.complete();
   }
 
-  
-
-  
-//captcha site key make sure to change this according to your domain (I used Localhost login on transco gmail)
 
   siteKey:string ="6Lfm5R4pAAAAAH8_jyMZ7AVsigdgQWzHiow3Q7a5";
 
   send(event: Event) {
-  
     event.preventDefault();
-  
-    // If the form is invalid, do not proceed with the API request
     if (this.form.invalid) {
       return;
     }
@@ -86,23 +63,16 @@ this.aFormGroup = this.formBuilder.group({
  
 }
 
-sendEmail() {
+sendEmail(): void {
   this._securityService.sendEmail(this.form.value.email)
     .pipe(takeUntil(this._onDestroy$))
     .subscribe({
       next: (data) => {
         if (!data) {
           return;
-        }
-        // Check additional conditions based on the response, if needed
-
-        // Handle success, e.g., update UI, show a success message, etc.
-     
-
-    
+        }    
       },
       error: (error) => {
-        // Handle error, e.g., show an error message, log the error, etc.
         console.error('Error sending email:', error);
       }
     });
--- src/app/pages/pages/auth/reset-password/reset-password-routing.module.ts
+++ src/app/pages/pages/auth/reset-password/reset-password-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { VexRoutes } from '../../../../../@vex/interfaces/vex-route.interface';
 import { ResetPasswordComponent } from './reset-password.component';
 import { RouterModule } from '@angular/router';
