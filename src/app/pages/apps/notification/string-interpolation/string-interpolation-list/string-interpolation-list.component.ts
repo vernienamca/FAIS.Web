33b3a5181de7ef1dee87df86d0a2bd94e963ed05
Vernie Namca <vernie.namca@theretailscore.com>
2024-01-23 19:16:10 +0800

    Minimal code fixes on string interpolation list

--- src/app/pages/apps/notification/string-interpolation/string-interpolation-list/string-interpolation-list.component.ts
+++ src/app/pages/apps/notification/string-interpolation/string-interpolation-list/string-interpolation-list.component.ts
@@ -1,28 +1,16 @@
-import {
-  AfterViewInit,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from "@angular/core";
-import { Observable, of, ReplaySubject, Subject } from "rxjs";
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { Observable, ReplaySubject, Subject } from "rxjs";
 import { filter, takeUntil } from "rxjs/operators";
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
-import { MatDialog } from "@angular/material/dialog";
 import { TableColumn } from "../../../../../../@vex/interfaces/table-column.interface";
 import { SelectionModel } from "@angular/cdk/collections";
 import { fadeInUp400ms } from "../../../../../../@vex/animations/fade-in-up.animation";
-import {
-  MAT_FORM_FIELD_DEFAULT_OPTIONS,
-  MatFormFieldDefaultOptions,
-} from "@angular/material/form-field";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from "@angular/material/form-field";
 import { stagger40ms } from "../../../../../../@vex/animations/stagger.animation";
 import { UntypedFormControl } from "@angular/forms";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { MatSelectChange } from "@angular/material/select";
 import { PortalService } from "src/app/core/services/portal.service";
 import { IStringInterpolation } from "src/app/core/models/string-interpolation";
 import { Router } from "@angular/router";
@@ -42,33 +30,18 @@ import { Router } from "@angular/router";
     },
   ],
 })
-export class StringInterpolationListComponent
-  implements OnInit, OnDestroy, AfterViewInit
-{
+export class StringInterpolationListComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   @Input()
   columns: TableColumn<IStringInterpolation>[] = [
-    {
-      label: "Code",
-      property: "transactionCode",
-      type: "text",
-      visible: true,
-      cssClasses: ["font-medium"],
-    },
-    {
-      label: "Description",
-      property: "description",
-      type: "text",
-      visible: true,
-    },
-    { label: "Actions", property: "actions", type: "button", visible: true },
+    { label: "Code", property: "transactionCode", type: "text", visible: true, cssClasses: ["font-medium"] },
+    { label: "Description", property: "description", type: "text", visible: true },
+    { label: "Actions", property: "actions", type: "button", visible: true }
   ];
 
   layoutCtrl = new UntypedFormControl("fullWidth");
-  subject$: ReplaySubject<IStringInterpolation[]> = new ReplaySubject<
-    IStringInterpolation[]
-  >(1);
+  subject$: ReplaySubject<IStringInterpolation[]> = new ReplaySubject<IStringInterpolation[]>(1);
   data$: Observable<IStringInterpolation[]> = this.subject$.asObservable();
   interpolations: IStringInterpolation[];
   totalCount: number = 0;
@@ -78,19 +51,17 @@ export class StringInterpolationListComponent
   selection = new SelectionModel<IStringInterpolation>(true, []);
   searchCtrl = new UntypedFormControl();
 
+  get visibleColumns() {
+    return this.columns.filter((column) => column.visible).map((column) => column.property);
+  }
+
   private _onDestroy$ = new Subject<void>();
 
   constructor(
-    private _dialog: MatDialog,
     private _portalService: PortalService,
     private _router: Router
   ) {}
 
-  get visibleColumns() {
-    return this.columns
-      .filter((column) => column.visible)
-      .map((column) => column.property);
-  }
   ngOnInit() {
     this._portalService.getStringInterpolation()
       .pipe(takeUntil(this._onDestroy$))
@@ -105,7 +76,6 @@ export class StringInterpolationListComponent
     this.data$
       .pipe(filter<IStringInterpolation[]>(Boolean))
       .subscribe((interpolations) => {
-        console.log(interpolations);
         this.totalCount = interpolations.length;
         this.interpolations = interpolations;
         this.dataSource.data = interpolations;
@@ -115,15 +85,18 @@ export class StringInterpolationListComponent
       .pipe(untilDestroyed(this))
       .subscribe((value) => this.onFilterChange(value));
   }
+
   ngOnDestroy(): void {
     this._onDestroy$.next();
     this._onDestroy$.complete();
   }
+
   ngAfterViewInit() : void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-  onFilterChange(value: string) : void{
+
+  onFilterChange(value: string) : void {
     if (!this.dataSource) {
       return;
     }
@@ -131,27 +104,26 @@ export class StringInterpolationListComponent
     value = value.toLowerCase();
     this.dataSource.filter = value;
   }
+
   toggleColumnVisibility(column, event): void {
     event.stopPropagation();
     event.stopImmediatePropagation();
     column.visible = !column.visible;
   }
-  isAllSelected() : boolean {
+
+  isAllSelected(): boolean {
     return this.selection.selected.length === this.dataSource.data.length;
   }
-  masterToggle() : void {
-    this.isAllSelected()
-      ? this.selection.clear()
-      : this.dataSource.data.forEach((row) => this.selection.select(row));
+
+  masterToggle(): void {
+    this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach((row) => this.selection.select(row));
   }
+
   trackByProperty<T>(index: number, column: TableColumn<T>) : string {
     return column.property;
   }
-  onLabelChange(change: MatSelectChange, row: IStringInterpolation) : void{
-    const index = this.interpolations.findIndex((c) => c === row);
-    this.subject$.next(this.interpolations);
-  }
-  createStringInterpolation() : void{
+
+  createStringInterpolation(): void{
     this._router.navigate(["apps/string-interpolation-add"]);
   }
 }
