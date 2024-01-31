2662095b349fe8578d76e59e1757ea6bdd6966ef
alvin-mybusybee <150876437+alvin-mybusybee@users.noreply.github.com>
2024-01-31 23:10:32 +0800

    changes in form fields

--- src/app/pages/apps/system-settings/components/app-version/app-version.component.ts
+++ src/app/pages/apps/system-settings/components/app-version/app-version.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject, of, takeUntil } from 'rxjs';
 
-export interface PeriodicElement {
+export interface IAppVersion {
   versionNo: number;
   date: Date;
   amendment: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: IAppVersion[] = [
   {versionNo: 1.1, date: new Date('7/28/2023'), amendment: 'Fixed issue on region filter'},
   {versionNo: 1.0, date: new Date('7/27/2023'), amendment: 'Initial Release'},
 ];
@@ -17,10 +18,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './app-version.component.html',
   styleUrls: ['./app-version.component.scss']
 })
-export class AppVersionComponent {
+export class AppVersionComponent implements OnInit {
   displayedColumns: string[] = ['version', 'date', 'amendment'];
   dataSource = ELEMENT_DATA;
   versionForm: FormGroup;
+  latestVersion:IAppVersion;
 
   get formControls() {
     return {
@@ -30,6 +32,8 @@ export class AppVersionComponent {
     }
   }
 
+  private _onDestroy$ = new Subject<void>();
+
   constructor(private _fb: FormBuilder) {
     this.versionForm = this._fb.group({
       versionNo: ['',Validators.required],
@@ -38,6 +42,13 @@ export class AppVersionComponent {
     });
   }
 
+  ngOnInit(): void {
+    of(this.dataSource)
+    .pipe(takeUntil(this._onDestroy$))
+    .subscribe((data:any) => {
+      this.latestVersion = data[0];
+    });
+  }
 
 
 }
